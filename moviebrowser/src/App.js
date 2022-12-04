a7ca90163f9b08cb9a8f8c426b6d3c0f094cb738
Janwry Dela Cruz <janwry.jrv@gmail.com>
2022-12-04 23:00:29 +0800

    practice on reactjs 201 with final project

--- moviebrowser/src/App.js
+++ moviebrowser/src/App.js
@@ -12,12 +12,14 @@ function App() {
 
 
   useEffect(() => {
-    
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=d06ada8fa071c4c50855b5867db2c26c&language=en-US&query=${searchText}&page=1&include_adult=false`)
-    .then(response => response.json())
-    .then(data => {
-      setSearchResults(data.results);
-    })
+    if(searchText){
+      
+      fetch(`https://api.themoviedb.org/3/search/movie?api_key=d06ada8fa071c4c50855b5867db2c26c&language=en-US&query=${searchText}&page=1&include_adult=false`)
+      .then(response => response.json())
+      .then(data => {
+        setSearchResults(data.results);
+      })
+    }
   }, [searchText])
   return (
     <div>
--- moviebrowser/src/components/Search.js
+++ moviebrowser/src/components/Search.js
@@ -1,13 +1,16 @@
 import Hero from './Hero'
+import MovieCard from './MovieCard';
 
-//tmdb API key d06ada8fa071c4c50855b5867db2c26c
-//example link for movie searches = https://api.themoviedb.org/3/search/company?api_key=d06ada8fa071c4c50855b5867db2c26c&query=spider%20man&page=1
 const Search = ({keyword, searchResults}) => {
-    const title = `You are searching for ${keyword}`
-    console.log(searchResults, 'are the search results')
+    const title = `You are searching for ${keyword}`;
+    const resultHtml = searchResults.map((obj,i) =>{
+      return <div key={i}>{obj.original_title}</div>
+    })
+
     return (
       <>
       <Hero text={title}/> 
+      {resultHtml}
       </>
     )
 }
