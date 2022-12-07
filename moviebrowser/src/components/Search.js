6561fc4d1c9779ca93b6ce37478a0f00289566f0
Janwry Dela Cruz <janwry.jrv@gmail.com>
2022-12-07 21:23:03 +0800

    practicing reactjs 201

--- moviebrowser/src/components/Search.js
+++ moviebrowser/src/components/Search.js
@@ -1,16 +1,40 @@
 import Hero from './Hero'
-import MovieCard from './MovieCard';
+
+
+const MovieCard = ({movie}) => {
+  const posterUrl = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+  return(
+  <div className="col-lg-3 col-md-3 col-2 my-4">
+    <div className="card">
+    <img src={posterUrl} className="card-img-top" alt={movie.original_title}/>
+    <div className="card-body">
+        <h5 className="card-title">{movie.original_title}</h5>
+        <a href="#" className="btn btn-primary">Show details</a>
+    </div>
+    </div>
+  </div>
+)
+}
 
 const Search = ({keyword, searchResults}) => {
     const title = `You are searching for ${keyword}`;
+
     const resultHtml = searchResults.map((obj,i) =>{
-      return <div key={i}>{obj.original_title}</div>
+     return <MovieCard movie={obj} key={i}/>
     })
 
     return (
       <>
       <Hero text={title}/> 
-      {resultHtml}
+      {resultHtml &&
+        <div className='container'>
+          <div className="row">
+            {resultHtml}
+          </div>
+        </div>
+      }
+
+
       </>
     )
 }
