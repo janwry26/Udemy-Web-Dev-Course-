327d297e903aafc421a7cd3aa944519ab31847da
Janwry Dela Cruz <janwry.jrv@gmail.com>
2022-12-10 21:32:45 +0800

    added trending list of movies on home

--- moviebrowser/src/components/Home.js
+++ moviebrowser/src/components/Home.js
@@ -1,19 +1,91 @@
 import Hero from './Hero';
+import { useParams } from 'react-router'
 
-const Home = () => {
-    return(
+import { Link } from 'react-router-dom'
+import { useState,useEffect } from 'react';
+
+const MovieCard=({movie})=>{
+
+  if(movie.poster_path){
+
+      var  posterUrl=`https://image.tmdb.org/t/p/w500/${movie.poster_path}` 
+
+  }else{
+
+      posterUrl=require("./img.jpg")
+  }
+
+  const detailUrl=`/moviedetail/${movie.id}`;
+
+
+
+  return(
       <>
-        <Hero text="Welcome to react component 201" />
-      <div className="container-fluid ">
-        <div className='row'>
-            <div className='col-lg-8 offset-lg-2 my-5'>
-              <p className='lead'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lacus enim, tempus vel aliquet nec, ultrices sed erat. Phasellus ultrices, lectus id cursus fringilla, erat orci tincidunt nunc, eu molestie nisi nulla eu tellus. Nam vel ornare sapien, quis imperdiet magna. Nam luctus ac risus eget mattis. Suspendisse potenti. Donec non condimentum nibh. Vivamus ac semper ligula. Nullam varius pretium lorem quis rhoncus. Praesent vitae porta ex, quis dignissim turpis. Nam ut tempus felis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla a leo pellentesque neque porta consectetur. Proin id ante nec felis porttitor posuere ac eu tellus. Proin tincidunt nulla nulla, quis pharetra velit convallis at. Phasellus lectus tellus, convallis eu viverra gravida, tristique ut purus. Quisque nec mauris lacinia, congue odio at, fermentum dolor.
-              </p>
-            </div>
-        </div>
+      <div className='col-4 col-md-6 col-lg-3 my-2'>
+          <Link to={detailUrl} style={{'textDecoration':'none'}} >
+              <div className="card h-100">
+                  <img src={posterUrl} className="card-img" alt={movie.original_title} />
+                  <div className="card-body">
+                      <p className="card-title text-truncate" >{movie.original_title}</p>
+                  </div>
+              </div>    
+          </Link>   
       </div>
       </>
-    )
-  }
+  )
+}
+
+
+function Home(){
+
+  const [trendList, setTrendList]=useState([])
+
+  const {id}=useParams
+
+  useEffect(()=>{
+      fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=d06ada8fa071c4c50855b5867db2c26c')
+      .then(response=>response.json())
+      .then(data=>{
+      setTrendList(data)
+      
+  })
+},[id])
+
+
+// console.log(trendList.results)
+
+let resultsHtml
+
+if(trendList.results){
+
+   resultsHtml=trendList.results.slice(0,12).map((obj)=><MovieCard key={obj.id}  movie={obj}/>)
+
+}
+
+
+
+
+      return(
+
+          <>
+          <Hero text="Welcome to movie browser"/>
+              
+         
+          
+
+        
+              <div className='col-lg-2'></div>
+                  <div className='container col-lg-8'>
+                      <h3 id='head' className='my-2 text-center'>Trending movies</h3>
+
+                      <div className='row'>
+                          {resultsHtml}
+                      </div>
+                  </div>
+                  <div className='col-lg-2'></div>
+
+          
+          </>
+      )
+}
   export default Home;  
