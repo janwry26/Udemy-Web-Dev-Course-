f6f76c9e329cc3dc319b8210e88d0e9763974e71
Janwry Dela Cruz <janwry.jrv@gmail.com>
2022-12-29 16:29:27 +0800

    practicing python 101 on udemy

---
+++ PYTHON FOLDER/lists.py
@@ -0,0 +1,6 @@
+lst = ["string", 1,3.14,["new item", "janwry"],"dela cruz"]
+names = ["janwry","Chritine","Joy"]
+#names.remove('Joy') remove Joy from the list
+#names.append('Pogi') add pogi from the last of the list
+names.reverse()
+print(names)
