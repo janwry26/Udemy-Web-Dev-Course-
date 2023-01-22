cc51b6ad43860e0125d9ea8b1ed6301dfc1b0ae9
Janwry Dela Cruz <janwry.jrv@gmail.com>
2023-01-22 17:07:02 +0800

    practice funciton on python

---
+++ PYTHON FOLDER/funciton.py
@@ -0,0 +1,14 @@
+# def firstFucntion(name, food):
+#     print(f"my name is{name}, and i love {food}")
+# firstFucntion("Janwry",'Egg')
+
+# def secondFunction():
+#     return "a value"
+# thing = secondFunction()
+# print(thing)
+
+def exp(num1, num2):
+    total = num1 + num2
+    return total
+bigNumber = exp(33,6)
+print(bigNumber)
