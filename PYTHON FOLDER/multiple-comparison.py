2bd853e841eb2bca8c378656ca7d345203fc0b3a
Janwry Dela Cruz <janwry.jrv@gmail.com>
2023-01-22 16:20:34 +0800

    practice multiple comparison in python

---
+++ PYTHON FOLDER/multiple-comparison.py
@@ -0,0 +1,13 @@
+# age = 20
+# name = "Janwry"
+# if age >= 20 and name == "Janwry":
+#     print("Pwede kana mag pakasal!")
+# else:
+#     print("Dipa pwede!") AND THE STATEMENT NEEDS TO BE TRUE 
+
+age = 20
+name = "Janwry"
+if age >= 20 or name == "Janwry":
+    print("Pwede kana mag pakasal!")
+else:
+    print("Dipa pwede!") #need one of them to be true
