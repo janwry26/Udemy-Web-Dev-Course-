a6d47975d8a7ac23322673a3676bf0c62674ae59
Janwry Dela Cruz <janwry.jrv@gmail.com>
2023-01-22 16:37:05 +0800

    Practicing for loop in python

---
+++ PYTHON FOLDER/for-loop.py
@@ -0,0 +1,17 @@
+# foods = ['Pizza','Burger','Egg']
+# food = set(foods)
+# for food in foods:
+#     # if food == "Pizza":
+#     #     size = input("What size would you like?")
+#     #     print(f"you ordered a {size} Pizza")
+#     print(food)
+#     food = "Pizza!"
+#     for letter in food:
+#         print(letter)
+   
+person ={
+    "name": "Janwry",
+    "ig": "@janwryy",
+}
+for key, value in person.items():
+    print(f"the key is {key} and the value is {value}")
