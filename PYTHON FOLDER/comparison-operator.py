bb737743b58030e3eeb5b9853120f84bec70252b
Janwry Dela Cruz <janwry.jrv@gmail.com>
2023-01-22 15:30:50 +0800

    learning python comparison operator

---
+++ PYTHON FOLDER/comparison-operator.py
@@ -0,0 +1,27 @@
+# do = True
+# if do == True:
+#     print("You can code")
+# else:
+#     print("you cant code right now!")
+# name = input("Please enter your name: ")
+# if name =="Janwry":
+#     print("Hi Janwry")
+#     order = "Pizza"
+# elif name =="CJ":
+#     order = "Shawarma"
+# else:
+#     print("wrong user")
+#     order = "Burger"
+# print(f"You are eating {order}")
+
+# name = input("Please enter your name: ")
+# name = name.lower()
+# if name != "bob":
+#     print("you are not bob")
+# else:
+#     print("you are bob")
+age = 18
+if age >= 18:
+    print("you are ready to vote!")
+else:
+    print("youre not qualified to vote")
